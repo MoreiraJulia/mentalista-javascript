c829871f8ba8232d7475a5c7865700c7f8883cef
Júlia Aparecida <julia.aparecida25@gmail.com>
2021-04-01 21:09:08 -0300

    Atualização do código

---
+++ js/funcoes.js
@@ -0,0 +1,19 @@
+let numeroSecreto = parseInt(Math.random() * 10);
+
+let tentativas = 3;
+
+while (tentativas > 0) {
+    let chute = parseInt(prompt("Digite um número entre 0 e 10: "));
+
+    if (chute == numeroSecreto) {
+        alert("Parabéns, você acertou!");
+        break;
+    } else if (numeroSecreto > chute) {
+        alert("O número secreto é maior que o chute");
+        tentativas = tentativas - 1;
+    } else if (numeroSecreto < chute) {
+        alert("O número secreto é menor que o chute");
+        tentativas = tentativas -1
+    } 
+}
+
